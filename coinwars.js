0cd1514c75b8fc92ba604317ac171d55a1227149
Steve Geluso <stevegeluso@gmail.com>
2014-10-02 18:05:08 -0700

    dimes flip coins

--- coinwars.js
+++ coinwars.js
@@ -20,11 +20,31 @@ function init() {
 function animate() {
     requestAnimFrame(animate);
 
-    /*
-    var coin = _.sample(STAGE.children);
-    coin.position.x = _.random(0, WIDTH);
-    coin.position.y = _.random(0, HEIGHT);
-    */
+    _.each(STAGE.children, function(coin1) {
+      coin1.position.x += coin1._meta.ax;
+      coin1.position.y += coin1._meta.ay;
+
+      _.each(STAGE.children, function(coin2) {
+        if ((coin1 !== coin2) && collision(coin1, coin2)) {
+          console.log("collision");
+          coin2._meta.ax = coin1._meta.ax;
+          coin2._meta.ay = coin1._meta.ay;
+
+          coin1._meta.ax = 0;
+          coin1._meta.ay = 0;
+
+          convert(coin1, coin2);
+          convert(coin2, coin1);
+          immobilize(coin1, coin2);
+          immobilize(coin2, coin1);
+        }
+
+        coin1._meta.ax *= .99;
+        coin1._meta.ay *= .99;
+
+        stop(coin1);
+      });
+    });
 
     RENDERER.render(STAGE);
 }
@@ -55,9 +75,15 @@ function createCoins() {
   createCoin("dime", "tails", 13/16, 7/9);
 }
 
-function createCoin(coin, team, x, y) {
-  var texture = PIXI.Texture.fromImage("img/" + coin + "_" + team + ".png");
+function createCoin(type, team, x, y) {
+  var texture = PIXI.Texture.fromImage("img/" + type + "_" + team + ".png");
   var coin = new PIXI.Sprite(texture);
+  coin._meta = {
+    type: type,
+    team: team,
+    ax: 0,
+    ay: 0
+  };
 
   // center the sprites anchor point
   coin.anchor.x = 0.5;
@@ -70,14 +96,58 @@ function createCoin(coin, team, x, y) {
   coin.setInteractive(true);
   coin.mousedown = function(mouseData){
      console.log("MOUSE DOWN!");
+     this._meta.x0 = Math.round(mouseData.global.x);
+     this._meta.y0 = Math.round(mouseData.global.y);
   }
    
   coin.mouseup = function(mouseData){
      console.log("MOUSE UP!");
+     this._meta.x1 = Math.round(mouseData.global.x);
+     this._meta.y1 = Math.round(mouseData.global.y);
+
+     this._meta.ax = this._meta.x0 - this._meta.x1;
+     this._meta.ay = this._meta.y0 - this._meta.y1;
+
+     this._meta.active = true;
   }
 
   STAGE.addChild(coin);
 }
 
+function distance(coin1, coin2) {
+  var dx = coin1.position.x - coin2.position.x;
+  var dy = coin1.position.y - coin2.position.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function collision(coin1, coin2) {
+  var d = distance(coin1, coin2);
+  var touching = (coin1.width / 2) + (coin2.width / 2);
+  return d <= touching;
+}
+
+function stop(coin) {
+  if (coin._meta.ax == 0 && coin._meta.ay == 0) {
+    coin._meta.active = false;
+  }
+}
+
+function convert(coin1, coin2) {
+  var type = coin2._meta.type;
+  if (type === "general" || type === "dime") {
+    return;
+  }
+  if (coin1._meta.active && coin1._meta.type === "dime") {
+    var filename = coin2._meta.type + "_" + coin1._meta.team + ".png";
+    var texture = PIXI.Texture.fromFrame("img/" + filename);
+    coin2._meta.team = coin2._meta.team;
+    coin2._meta.type = coin2._meta.type;
+    coin2.setTexture(texture);
+  }
+}
+
+function immobilize(coin1, coin2) {
+
+}
 
 $(document).ready(init);
